046fd7df2aee0aaaf2a027824407965706474aa4
Cedric <cedmanchau@gmail.com>
2024-07-31 22:57:39 -0700

    s

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -17,7 +17,31 @@ export default function sitemap(): MetadataRoute.Sitemap {
       },
     },
     {
+      url: 'https://edtechquity.net/es',
+      lastModified: new Date(),
+      changeFrequency: 'weekly',
+      priority: 1,
+      alternates: {
+        languages: {
+          en: 'https://edtechquity.net',
+          es: 'https://edtechquity.net/es',
+        },
+      },
+    },
+    {
       url: 'https://edtechquity.net/summit-2024',
+      lastModified: new Date(),
+      changeFrequency: 'weekly',
+      priority: 0.8,
+      alternates: {
+        languages: {
+          en: 'https://edtechquity.net/summit-2024',
+          es: 'https://edtechquity.net/es/summit-2024',
+        },
+      },
+    },
+    {
+      url: 'https://edtechquity.net/es/summit-2024',
       lastModified: new Date(),
       changeFrequency: 'weekly',
       priority: 0.8,
@@ -41,6 +65,18 @@ export default function sitemap(): MetadataRoute.Sitemap {
       },
     },
     {
+      url: 'https://edtechquity.net/es/about-us',
+      lastModified: new Date(),
+      changeFrequency: 'weekly',
+      priority: 0.5,
+      alternates: {
+        languages: {
+          en: 'https://edtechquity.net/about-us',
+          es: 'https://edtechquity.net/es/about-us',
+        },
+      },
+    },
+    {
       url: 'https://edtechquity.net/faq',
       lastModified: new Date(),
       changeFrequency: 'weekly',
@@ -53,7 +89,31 @@ export default function sitemap(): MetadataRoute.Sitemap {
       },
     },
     {
+      url: 'https://edtechquity.net/es/faq',
+      lastModified: new Date(),
+      changeFrequency: 'weekly',
+      priority: 0.5,
+      alternates: {
+        languages: {
+          en: 'https://edtechquity.net/faq',
+          es: 'https://edtechquity.net/es/faq',
+        },
+      },
+    },
+    {
       url: 'https://edtechquity.net/sponsorship',
+      lastModified: new Date(),
+      changeFrequency: 'weekly',
+      priority: 0.5,
+      alternates: {
+        languages: {
+          en: 'https://edtechquity.net/sponsorship',
+          es: 'https://edtechquity.net/es/sponsorship',
+        },
+      },
+    },
+    {
+      url: 'https://edtechquity.net/es/sponsorship',
       lastModified: new Date(),
       changeFrequency: 'weekly',
       priority: 0.5,
