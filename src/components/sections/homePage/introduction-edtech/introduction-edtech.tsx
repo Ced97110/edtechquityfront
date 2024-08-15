38004d451810bdc4656dce041847ba4039efa916
Cedric <cedmanchau@gmail.com>
2024-08-15 14:29:57 -0700

    s

--- src/components/sections/homePage/introduction-edtech/introduction-edtech.tsx
+++ src/components/sections/homePage/introduction-edtech/introduction-edtech.tsx
@@ -54,7 +54,7 @@ import LanguageChanger from '@components/utils/LanguageChanger';
                 <div className='lg:col-span-2 col-span-3 pb-6'>
                      <video
                         key={i18n.language}
-                        src={i18n.language === 'en' ? '/t1.mp4' : '/test1.mp4'}
+                        src={i18n.language === 'en' ? '/t1.mp4' : '/tspanish.mp4'}
                         autoPlay={true}
                         loop
                         muted
